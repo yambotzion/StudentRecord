ddfd4feed4e906eff39486578c2e48e4ad0f2df9
John Kim <jkim@rowlandschools.org>
2019-10-17 07:40:47 -0700

    Initial Upload: Version 0.1
    This is the initial upload for students.

---
+++ StudentRecord.java
@@ -0,0 +1,38 @@
+public class StudentRecord
+{
+   //contains scores.length values and scores.length > 1
+   private int [] scores;
+   
+   //Constructs a StudentRecord object with an array of scores.
+   public StudentRecord(int [] s)
+   {
+      scores = s;
+   }
+   
+   /** returns the average (arithmetic mean) of the values in scores
+    *  whose subscripts are between first and last, inclusive
+    *  PRECONDITION: 0 <= first <= last < scores.length
+    */
+   private double average(int first, int last)
+   {
+      return 0; //here so the class compiles
+   }
+    
+   /** returns true if each successive value in scores is greater than
+    *  or equal to the previous value; false otherwise.
+    */
+   private boolean hasImproved()
+   {
+      return false; //here so the class compiles
+   }  
+   
+   /** if the values in scores have imrpoved, returns the average of
+    *  the elements in scores with indexes greater than or equal to 
+    *  scores.length/2; otherwise, returns the average of all the 
+    *  values in scores
+    */
+   public double finalAverage()
+   {
+      return 0; //here so the class compiles
+   } 
+}
---
+++ StudentRecordTester.java
@@ -0,0 +1,15 @@
+
+/**
+ * Driver for the class StudentRecord
+ *
+ * @JKim 
+ * @version (0.1)
+ */
+public class StudentRecordTester
+{
+    public static void main(String[] args)
+    {
+        StudentRecord[] students;
+    }
+    
+}
