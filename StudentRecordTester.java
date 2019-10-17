04b15a41a966cffe20b33f684eb217569d0fb45a
John Kim <jkim@rowlandschools.org>
2019-10-17 08:00:56 -0700

    Added grades
    Added 3 integer arrays representing grades.

--- StudentRecordTester.java
+++ StudentRecordTester.java
@@ -9,6 +9,10 @@ public class StudentRecordTester
 {
     public static void main(String[] args)
     {
+        int[] a={50,50,20,80,30};
+        int[] b={20,50,50,53,80};
+        int[] c={20,50,50,80};
+        
         StudentRecord[] students;
     }
     
